1bd38d49f07b387cd2f0bc4d802d29d0fc9b21dc
Shivaniiiiiiiiiiiii <shivanipatil595@gmail.com>
2025-02-18 18:27:52 +0530

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UploadFiles from './Components/UploadFiles'
+
+function App() {
+
+  return (
+    <>
+      <UploadFiles />
+    </>
+  )
+}
+
+export default App
